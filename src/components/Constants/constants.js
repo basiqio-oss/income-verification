4052de1adaa76fe5c69ebb47e302a76727c82f8d
Ashman Malik <ashman@basiq.io>
2024-10-22 15:50:13 +1000

    DX-854 IV Code Refactoring

--- src/components/Constants/constants.js
+++ src/components/Constants/constants.js
@@ -3,3 +3,5 @@ export const COOKIES_TOKEN = 'cookies_token';
 export const LOCAL_STORAGE_USER_EMAIL = "USER_EMAIL";
 export const LOCAL_STORAGE_TOKEN = "BASI_Q_TOKEN";
 export const LOCAL_STORAGE_JOB_ID = "JOB_ID";
+export const COOKIES_USER_EMAIL = 'cookies_user_email'; 
+export const COOKIES_JOB_ID = 'cookies_job_id'; 
